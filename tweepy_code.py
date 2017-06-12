ede302d786184400dfa9db62b8b97c96b8d95d76
Jye <jyesawtellrickson@gmail.com>
2017-06-12 15:13:06 +0800

    No limit tweet history

--- tweepy_code.py
+++ tweepy_code.py
@@ -47,6 +47,7 @@ def tweets_for_user(api, username):
         tweets += [status._json]
     # check the user is legit before returning
     # are they really a thought leader? or jobs
+    # check bio as well, does it have our keywords?
     test_edu = 0
     test = 0
     for tweet in tweets:
@@ -59,6 +60,14 @@ def tweets_for_user(api, username):
         return
     return tweets
 
+def limit_handled(cursor):
+    while True:
+        try:
+            yield cursor.next()
+        except: # tweepy.RateLimitError:
+            print("hit rate limit")
+            break
+
 
 # need to add the use of since_id
 def tweets_for_search(api, search, number):
@@ -66,8 +75,12 @@ def tweets_for_search(api, search, number):
         number = 150
     tweets = []
     # return results up to number
-    for status in tweepy.Cursor(api.search, q=search).items(number):
+    for status in limit_handled(tweepy.Cursor(api.search, q=search).items()):
         tweets += [status._json]
+        # print updates to screen
+        if len(tweets) % 100 == 0:
+            print("{0} tweets processed".format(len(tweets)))
+            save_file(tweets, "during_processing.pkl")
     return tweets
 
 
@@ -103,6 +116,23 @@ def year_from_date(date_string):
     return datetime.strptime(date_string, '%a %b %d %H:%M:%S %z %Y').year
 
 
+def plot_historical(historical_data):
+    """
+    Take historical data as dict of terms and plot a graph
+
+    Args:
+        historical_data:
+
+    Returns:
+
+    """
+    for term in historical_data.keys():
+        plt.plot([t[0] for t in historical_data[term]], [t[1] for t in historical_data[term]], label=term)
+    plt.legend()
+    plt.show()
+    return
+
+
 def history_generator(tweets):
     """
     Create a dict of terms with frequency
@@ -118,12 +148,13 @@ def history_generator(tweets):
     counts_all = {}
     for year in range(start_year, end_year+1):
         process_tweets = [t[1] for t in tweets_td if year_from_date(t[0]) == year]
-        counts = create_counts(process_tweets)
-        for term in counts:
-            if term in counts_all.keys():
-                counts_all[term] += [(year, counts[term])]
-            else:
-                counts_all[term] = [(year, counts[term])]
+        if process_tweets != []:
+            counts = create_counts(process_tweets)
+            for term in counts:
+                if term in counts_all.keys():
+                    counts_all[term] += [(year, counts[term])]
+                else:
+                    counts_all[term] = [(year, counts[term])]
     return counts_all
 
 
@@ -145,10 +176,7 @@ def create_counts(doc):
     return fdist
 
 
-def get_tweets():
-    print("getting tweets for search term")
-    HISTORICAL_TWEETS = tweets_for_search(api, "#beauty", None)
-    # print([t.text for t in HISTORICAL_TWEETS])
+def get_user_tweets(HISTORICAL_TWEETS):
     # get a count of user strings
     users = dict(Counter([t['user']['id_str'] for t in HISTORICAL_TWEETS]))
     top_10 = sorted(users, key=users.get, reverse=True)[:20]
@@ -158,7 +186,22 @@ def get_tweets():
         new_tweets = tweets_for_user(api, user_id_str)
         HISTORICAL_TWEETS += new_tweets
 
-    output_file = open('historical_tweets.pkl', 'wb')
+    return HISTORICAL_TWEETS
+
+
+def save_file(file, output_name):
+    output_file = open(output_name, 'wb')
+    pickle.dump(file, output_file)
+    output_file.close()
+    return
+
+
+def get_tweets():
+    print("getting tweets for search term")
+    HISTORICAL_TWEETS = tweets_for_search(api, "#beauty", None)
+    # print([t.text for t in HISTORICAL_TWEETS])
+
+    output_file = open('historical_tweets_2.pkl', 'wb')
 
     pickle.dump(HISTORICAL_TWEETS, output_file)
 
@@ -168,7 +211,7 @@ def get_tweets():
 def analyse_tweets():
     tweets_file = open('historical_tweets.pkl','rb')
     tweets = pickle.load(tweets_file)
-    print(history_generator(tweets))
+    plot_historical(history_generator(tweets))
     tweets_dt = tweets_to_text_date(tweets)
     # create old and new doc
     doc = " ".join([str(i[1]) for i in tweets_dt])
@@ -182,7 +225,7 @@ def analyse_tweets():
     generate_wordcloud(doc)
 
 
-# get_tweets()
+get_tweets()
 analyse_tweets()
 # done
 
