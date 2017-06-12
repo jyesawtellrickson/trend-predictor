0a44f12fae1de7cdd81f62372e9eaecafac63565
Jye <jyesawtellrickson@gmail.com>
2017-06-12 12:09:25 +0800

    Historical features added

--- tweepy_code.py
+++ tweepy_code.py
@@ -2,9 +2,14 @@ import tweepy
 from tweepy import OAuthHandler
 
 from collections import Counter
+from datetime import datetime
 
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
+
+from sklearn.feature_extraction.text import CountVectorizer
+import numpy as np
+import operator
 
 import json
 import pickle
@@ -18,19 +23,13 @@ api = tweepy.API(auth)
 
 HISTORICAL_TWEETS = []
 
-def process_or_store(tweet, HISTORICAL_TWEETS):
-    #print(json.dumps(tweet))
-    HISTORICAL_TWEETS += [json.dumps(tweet)]
-    return HISTORICAL_TWEETS
-
-
-
 # get tweets on the education hashtag
 # count the number of times each user has tweeted on that hashtag
 # get the tweets of the most common user, update score table with rt's and tags
 # potentially also get following / friends from our top users to grow our education
 # influencers
 # continue getting tweets
+# should preprocess the text lemmatize/stem
 
 """
 functions required:
@@ -51,8 +50,8 @@ def tweets_for_user(api, username):
     test_edu = 0
     test = 0
     for tweet in tweets:
-        test += tweet.text.find('job') >= 0
-        test_edu += tweet.text.find('educat') >= 0
+        test += tweet['text'].find('job') >= 0
+        test_edu += tweet['text'].find('educat') >= 0
     test = test / len(tweets)
     # do they talk about education enough?
     # are they posting too often?
@@ -100,12 +99,58 @@ def generate_wordcloud(doc):
     return
 
 
+def year_from_date(date_string):
+    return datetime.strptime(date_string, '%a %b %d %H:%M:%S %z %Y').year
+
+
+def history_generator(tweets):
+    """
+    Create a dict of terms with frequency
+    per month/year.
+
+    Returns: dictionary containing terms with tuples of counts / year
+
+    """
+    # convert tweets to date and time
+    tweets_td = tweets_to_text_date(tweets)
+    start_year = 2010
+    end_year = 2017
+    counts_all = {}
+    for year in range(start_year, end_year+1):
+        process_tweets = [t[1] for t in tweets_td if year_from_date(t[0]) == year]
+        counts = create_counts(process_tweets)
+        for term in counts:
+            if term in counts_all.keys():
+                counts_all[term] += [(year, counts[term])]
+            else:
+                counts_all[term] = [(year, counts[term])]
+    return counts_all
+
+
+def create_counts(doc):
+    """
+    Create a frequency distribution from a list of tweets.
+
+    Args:
+        doc: list of tweets
+
+    Returns: dict containing counts of top 100 words
+
+    """
+    cv = CountVectorizer(stop_words='english', max_features=100)
+    X = cv.fit_transform(doc)
+    freq = np.ravel(X.sum(axis=0))
+    vocab = [v[0] for v in sorted(cv.vocabulary_.items(), key=operator.itemgetter(1))]
+    fdist = dict(zip(vocab, freq))
+    return fdist
+
+
 def get_tweets():
-    HISTORICAL_TWEETS = tweets_for_search(api, "#education", 1500)
+    print("getting tweets for search term")
+    HISTORICAL_TWEETS = tweets_for_search(api, "#beauty", None)
     # print([t.text for t in HISTORICAL_TWEETS])
     # get a count of user strings
     users = dict(Counter([t['user']['id_str'] for t in HISTORICAL_TWEETS]))
-
     top_10 = sorted(users, key=users.get, reverse=True)[:20]
 
     for user_id_str in top_10:
@@ -118,10 +163,12 @@ def get_tweets():
     pickle.dump(HISTORICAL_TWEETS, output_file)
 
     output_file.close()
+    return
 
 def analyse_tweets():
     tweets_file = open('historical_tweets.pkl','rb')
     tweets = pickle.load(tweets_file)
+    print(history_generator(tweets))
     tweets_dt = tweets_to_text_date(tweets)
     # create old and new doc
     doc = " ".join([str(i[1]) for i in tweets_dt])
@@ -135,5 +182,7 @@ def analyse_tweets():
     generate_wordcloud(doc)
 
 
-
+# get_tweets()
 analyse_tweets()
+
+
