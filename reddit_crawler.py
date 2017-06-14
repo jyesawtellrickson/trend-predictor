8fec52975383e7f7d6202aa72e39dcd14481b6ad
Jye <jyesawtellrickson@gmail.com>
2017-06-14 21:16:54 +0800

    Added static methods

--- reddit_crawler.py
+++ reddit_crawler.py
@@ -20,10 +20,12 @@ from datetime import datetime
 
 class RedditCrawler():
 
-    def __init__(self, subreddits, dates):
+    def __init__(self, subreddits, dates, reddit_file='reddit_file.pkl', snippet_file='reddit_snippet_file.pkl'):
         # initialise basic props
         self.subreddits = subreddits
         self.dates = dates
+        self.reddit_file = reddit_file
+        self.snippet_file = snippet_file
 
     def authenticate(self, client_id, secret):
         """
@@ -35,12 +37,16 @@ class RedditCrawler():
         Returns:
 
         """
-        self.reddit = praw.Reddit(user_agent='Submission extraction',
-                                  client_id=client_id,
-                                  client_secret=secret)
+        try:
+            self.reddit = praw.Reddit(user_agent='Submission extraction',
+                                      client_id=client_id,
+                                      client_secret=secret)
+            print("Client authenticated")
+        except:
+            print("Client failed to authenticate")
         return
 
-    def get_comments(self, output='reddit_docs.pkl'):
+    def get_comments(self):
         """
         For the defined subreddits and dates, downloads
         all top-level comments and saves to specified file.
@@ -53,28 +59,27 @@ class RedditCrawler():
         if not self.subreddits:
             print("Must set subreddits first")
             return
-        self.output = output
+        submissions = []
         for subreddit in self.subreddits:
-            submissions = []
-            for submission in self.reddit.subreddit(subreddit).submissions(start=year_to_epoch(self.dates[0]),
-                                                              end=year_to_epoch(self.dates[1])
-                                                              ):
+            for submission in self.reddit.subreddit(subreddit).submissions(
+                    start=self.year_to_epoch(self.dates[0]),
+                    end=self.year_to_epoch(self.dates[1])):
                 # process submission
                 submissions += [(submission.id, submission.created, submission.title, submission.num_comments,
                          submission.ups, submission.downs)]
                 try:
-                    submissions += get_all_comments(submission)
+                    submissions += self.get_all_comments(submission)
                 except:
                     print("comments error")
                 print("Current date: ", datetime.fromtimestamp(submission.created).strftime('%Y %m %d'))
-            # dump completed output to pickle file
-            save_file(submissions, output)
             print(subreddit+" complete.")
+        # dump completed output to pickle file
+        save_file(submissions, self.reddit_file)
         # send final output to user
         print("All subreddits completed.")
         return
 
-    def get_submissions(self, output='reddit_docs.pkl'):
+    def get_submissions(self):
         """
         For the defined subreddits and dates, downloads
         all submissions and saves to specified file.
@@ -84,27 +89,26 @@ class RedditCrawler():
         Returns:
 
         """
-        self.output = output
         if not self.subreddits:
             print("Must set subreddits first")
             return
+        submissions = []
         for subreddit in self.subreddits:
-            submissions = []
-            for submission in self.reddit.subreddit(subreddit).submissions(start=year_to_epoch(self.dates[0]),
-                                                              end=year_to_epoch(self.dates[1])
-                                                              ):
+            for submission in self.reddit.subreddit(subreddit).submissions(
+                    start=self.year_to_epoch(self.dates[0]),
+                    end=self.year_to_epoch(self.dates[1])):
                 # process submission
                 submissions += [(submission.id, submission.created, submission.title, submission.num_comments,
-                         submission.ups, submission.downs)]
+                                 submission.ups, submission.downs)]
                 print("Current date: ", datetime.fromtimestamp(submission.created).strftime('%Y %m %d'))
-            # dump completed output to pickle file
-            save_file(submissions, output)
             print(subreddit+" complete.")
+        # dump completed output to pickle file
+        save_file(submissions, self.reddit_file)
         # send final output to user
         print("All subreddits completed.")
         return
 
-    def prepare_snippets(self, file_name=self.output, output='reddit_for_analysis.pkl'):
+    def prepare_snippets(self):
         """
         Converts reddit docs to Snippet form for
         processing in main script.
@@ -115,16 +119,18 @@ class RedditCrawler():
         Returns:
 
         """
-        submissions = load_file(file_name)
+        submissions = load_file(self.reddit_file)
         snippets = [(s[1], s[2], round(math.log(s[3]+1))+1) for s in submissions]
-        save_file(snippets, output)
-        print("Final file saved to ", output)
+        save_file(snippets, self.snippet_file)
+        print("Final file saved to ", self.snippet_file)
         return
 
-    def year_to_epoch(self, year):
+    @staticmethod
+    def year_to_epoch(year):
         return datetime.strptime(year, '%Y').timestamp()
 
-    def get_all_comments(self, submission, limit=0):
+    @staticmethod
+    def get_all_comments(submission, limit=0):
         """
         Return all comments for a submission.
         Args:
@@ -148,6 +154,8 @@ reddit_crawler = RedditCrawler(['education'], ['2015', '2016'])
 
 reddit_crawler.authenticate(reddit_client_id, reddit_secret)
 
-reddit_crawler.get_submissions('test.pkl')
+reddit_crawler.get_submissions()
+
+reddit_crawler.prepare_snippets()
 
 
